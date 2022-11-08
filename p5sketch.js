cf1f233c704cfedbec5854815423ef09ca9c0835
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2022-11-07 19:24:09 -0500

    p5

--- p5sketch.js
+++ p5sketch.js
@@ -1,24 +1,10 @@
-make_base();
-setup();
-draw();
-
-function make_base()
-{
-  base_image = new Image();
-  base_image.src = 'https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/22399837/original/1ff0d806f14ac2e69f78783b2e6624f4a872c454/draw-sweetness-illustrations-for-childrens.jpg';
-  base_image.onload = function(){
-    context.drawImage(base_image, 0, 0);
-  }
-}
-
 function setup() {
-    createCanvas(500, 500);
-}
-  
-function draw() {
-    if (mouseIsPressed) {
-      ellipse(mouseX, mouseY, 5, 5);
-      fill(0);
-    } 
+  createCanvas(1100, 741);
 }
 
+function draw() {
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, 5, 5);
+    fill(0);
+  } 
+}
