2c6d1fd9f5e8f0c4020dbb64f149298b32f19cb0
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2022-12-13 19:42:53 -0500

    final

--- final_project/final.js
+++ final_project/final.js
@@ -1,22 +1,20 @@
-//https://www.youtube.com/watch?v=7JtLHJbm0kA&t=14s&ab_channel=Frankslaboratory
-alert('hit -> or <- arrows');  
-
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = 1270;
     canvas.height = 567;
+    let changePage = 0;
 
     class InputHandler { //handles user input
         constructor(){
             this.keys = []; //keeps track of button presses
             window.addEventListener('keydown', e => { //ES6 arrow function (lexical scoping: dont bind their own 'this' but they inherit the one from their parents scope)
-                if((e.key === 'ArrowLeft' || e.key === 'ArrowRight') && this.keys.indexOf(e.key) === -1){ //dont store button press if alrdy in array  
+                if((e.key === 'ArrowLeft' || e.key === 'ArrowRight' || e.key === 'ArrowUp') && this.keys.indexOf(e.key) === -1){ //dont store button press if alrdy in array  
                     this.keys.push(e.key);
                 }
             });
             window.addEventListener('keyup', e => { //when was down and key is released, remove from the array
-                if(e.key === 'ArrowLeft'|| e.key === 'ArrowRight') {
+                if(e.key === 'ArrowLeft'|| e.key === 'ArrowRight' || e.key === 'ArrowUp') {
                     this.keys.splice(this.keys.indexOf(e.key), 1);
                 }
             });
@@ -27,8 +25,8 @@ window.addEventListener('load', function(){
         constructor(gameWidth, gameHeight){
             this.gameWidth = gameWidth;
             this.gameHeight = gameHeight;
-            this.width = 200;
-            this.height = 200;
+            this.width = 120;
+            this.height = 207;
             this.x = 0;
             this.y = this.gameHeight- this.height;
             this.image = document.getElementById('playerImage');
@@ -42,12 +40,40 @@ window.addEventListener('load', function(){
             context.drawImage(this.image, this.frameX*this.width, this.frameY*this.height, this.width, this.height, this.x, this.y, this.width, this.height);
         }
         update(input){
+            //collision detection w door
+            if(this.x == 1100){
+                this.x = 0;
+                changePage ++;
+            }
+
             this.x += this.speed;
             //movement w arrows
             if (input.keys.indexOf('ArrowRight') > -1){
-                this.speed = 5;
+                if (changePage == 1){ //dhaka page
+                    this.image = document.getElementById('playerImage')//add teacup on poem cover page
+                    this.speed = 5;
+                } else if (changePage == 2){ //poem cover page
+                    this.image = document.getElementById('playerTea')//add teacup on poem cover page 
+                    this.speed = 5;
+                } else if (changePage == 3){ //poem page
+                    this.image = document.getElementById('playerImage');
+                    this.speed = 5;
+                } else{
+                    this.image = document.getElementById('playerImage');
+                    this.speed = 5;
+                }
+
             } else if (input.keys.indexOf('ArrowLeft') > -1){
-                this.speed = -5;
+                this.image = document.getElementById('playerMirrorImage');
+                if (changePage == 1){ //dhaka page
+                    this.speed = -5;
+                } else {
+                    this.speed = -5;
+                }
+                
+            } else if (input.keys.indexOf('ArrowUp') > -1){
+                this.speed = 0;
+                this.image = document.getElementById('playerUpImage');
             } else {
                 this.speed = 0;
             }
@@ -58,11 +84,11 @@ window.addEventListener('load', function(){
         constructor(gameWidth, gameHeight){
             this.gameWidth = gameWidth;
             this.gameHeight = gameHeight;
-            this.image = document.getElementById('backgroundImage');
+            this.image = document.getElementById('back1');
             this.x = 0;
             this.y = 0;
             this.width = 1270;
-            this.height = 633;
+            this.height = 567;
             this.speed = 0;
         }
         draw(context){
@@ -70,14 +96,86 @@ window.addEventListener('load', function(){
             context.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
         }
         update(){
-            this.x -= this.speed;
-            //movement w arrows
-            if (input.keys.indexOf('ArrowRight') > -1){
-                this.speed = 10;
-            } else {
-                this.speed = 0;
+            //page 1: dhaka
+            if (changePage == 1){
+                this.width = 1270;
+                this.image = document.getElementById('back2');
+                //movement w arrows
+                this.x -= this.speed;
+                if (input.keys.indexOf('ArrowRight') > -1){
+                    this.speed = 11;
+                } else {
+                    this.speed = 0;
+                }
+                if(this.x < 0 - this.width) this.x = 0;
             }
-            if(this.x < 0 - this.width) this.x = 0;
+
+            //page 2: poem cover
+            if (changePage == 2){
+                this.width = 1270;
+                this.image = document.getElementById('magic1');
+                this.x = 0;
+                this.y = 0;
+            }
+
+            //page 3: poem 
+            if (changePage == 3 ){
+                this.width = 3810;
+                this.image = document.getElementById('poem');
+
+                //movement w arrows
+                this.x -= this.speed;
+                if (input.keys.indexOf('ArrowRight') > -1){
+                    this.speed = 11.55;
+                } else {
+                    this.speed = 0;
+                }
+                if(this.x < 0 - this.width) this.speed = 0;
+            }
+
+            //page 4: fashion cover
+            if (changePage == 4){
+                this.width = 1270;
+                this.image = document.getElementById('fashion');
+                this.x = 0;
+                this.y = 0;
+            }
+
+            //page 5: fashion and history
+            if (changePage == 5 || changePage == 6){
+                this.width = 7620;
+                this.image = document.getElementById('fashionhistory');
+
+                //movement w arrows
+                this.x -= this.speed;
+                if (input.keys.indexOf('ArrowRight') > -1){
+                    this.speed = 14.4;
+                } else {
+                    this.speed = 0;
+                }
+                if(this.x < 0 - this.width) this.speed = 0;
+            }
+            
+            //reset
+            if(changePage ==  7){changePage = 1;} 
+        }
+    }
+
+    class Door{
+        constructor(gameWidth, gameHeight){
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.width = 131;
+            this.height = 220;
+            this.x = 0;
+            this.y = this.gameHeight- this.height;
+            this.image = document.getElementById('door');
+            this.frameX = 0;
+            this.frameY = 0;
+            this.speed = 0;
+        }
+        draw(context){
+            context.drawImage(this.image, this.frameX*this.width, this.frameY*this.height, this.width, this.height, 1000, this.y, this.width, this.height);
         }
     }
 
@@ -88,13 +186,15 @@ window.addEventListener('load', function(){
     const input = new InputHandler();
     const player = new Player(canvas.width, canvas.height);
     const background = new Background(canvas.width, canvas.height);
+    const door = new Door(canvas.width, canvas.height);
 
     function animate(){ //animates screen
         ctx.clearRect(0,0,canvas.width,canvas.height);
         background.draw(ctx);
         background.update();
         player.draw(ctx);
-        player.update(input);
+        player.update(input,door);
+        //door.draw(ctx);
         requestAnimationFrame(animate);
     }
     animate();
